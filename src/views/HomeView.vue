87dabe4028830ba9c2677f9621730c6d2698e12a
Ghazi Nakkash <ghazi.nakkash@student.htw-Berlin.de>
2023-11-07 17:32:08 +0100

    Vue Hello World Done!

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="wishlist" src="../assets/wishlist.png">
+    <HelloWorld msg="Hello ShoppingList"/>
+    <NavBar></NavBar>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    NavBar,
+    HelloWorld
+  }
+}
+</script>
