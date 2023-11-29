723fc6e48cb700d9b7c0815d877c9b343de3b116
RitaChiblaq <s0588480@htw-berlin.de>
2023-11-29 22:40:58 +0100

    dummy shoppinglist app

---
+++ src/components/ShoppingList.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="container" style="max-width: 600px">
+    <h2 class="text-center mt-3 mb-4">My Shopping List</h2>
+
+    <!-- Input -->
+    <div class="d-flex justify-content-center align-items-center mb-3">
+      <input
+          type="text"
+          class="form-control"
+          v-model="newItem"
+          placeholder="Add new item"
+          @keyup.enter="submitItem"
+      />
+      <button @click="submitItem" class="btn btn-success rounded-0 ml-2">Add</button>
+    </div>
+
+    <!-- Task table -->
+    <table class="table table-bordered table-striped table-dark">
+      <thead>
+      <tr>
+        <th scope="col">Item</th>
+        <th scope="col" class="text-center">Actions</th>
+        <th scope="col" class="text-center"></th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(item, index) in items" :key="index">
+        <td>
+          <div class="d-flex align-items-center">
+            <input
+                type="checkbox"
+                :checked="item.status"
+                @change="changeStatus(index)"
+                class="mr-2"
+            />
+            <span :class="{ 'line-through': item.status }">{{ item.name }}</span>
+          </div>
+        </td>
+        <td>
+          <div class="text-center">
+            <button @click="editItem(index)" class="btn btn-primary btn-sm">Edit</button>
+          </div>
+        </td>
+        <td>
+          <div class="text-center">
+            <button @click="deleteItem(index)" class="btn btn-danger btn-sm">Delete</button>
+          </div>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShoppingList',
+  data() {
+    return {
+      newItem: '',
+      items: [
+        { name: 'banana', status: true }, // 'true' represents 'not-Empty'
+        { name: 'apple', status: false } // 'false' represents 'Empty'
+      ],
+      editedItem: null
+    };
+  },
+
+  methods: {
+    submitItem() {
+      if (this.newItem.trim() === '') return;
+
+      if (this.editedItem !== null) {
+        this.items[this.editedItem].name = this.newItem.trim();
+        this.editedItem = null;
+      } else {
+        this.items.push({
+          name: this.newItem.trim(),
+          status: false // Initial status is set to 'Empty'
+        });
+      }
+
+      this.newItem = '';
+    },
+
+    editItem(index) {
+      this.newItem = this.items[index].name;
+      this.editedItem = index;
+    },
+
+    deleteItem(index) {
+      this.items.splice(index, 1);
+    },
+
+    changeStatus(index) {
+      // Toggle the status of the item at the given index
+      this.items[index].status = !this.items[index].status;
+    }
+    // ... (other methods)
+  }
+};
+</script>
+
+
+<style scoped>
+.container {
+  max-width: 600px;
+  margin: auto;
+}
+
+.btn-sm {
+  padding: 0.2rem 0.5rem;
+  font-size: 0.8rem;
+}
+
+.line-through {
+  text-decoration: line-through;
+}
+
+/* Add some margin to the checkbox */
+input[type="checkbox"] {
+  margin-right: 5px;
+}
+</style>
