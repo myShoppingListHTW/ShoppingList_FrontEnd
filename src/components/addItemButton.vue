801fd774b8764186a833ddf49f971a1e10ce6a43
Ghazi Nakkash <ghazi.nakkash@student.htw-Berlin.de>
2023-12-29 18:23:28 +0100

    MHS-24 <Frontend Input validation>

--- src/components/addItemButton.vue
+++ src/components/addItemButton.vue
@@ -4,8 +4,12 @@
     <div v-if="popupVisible" class="popup">
       <form @submit.prevent="addItem">
         <label for="itemName">Item's Name:</label>
-        <input type="text" id="itemName" v-model="itemName" required />
-
+        <div class="input-container">
+          <input type="text" id="itemName" v-model="itemName" maxlength="20" required/>
+          <div v-if="!isNameValid" class="error-cloud" @click="closeErrorMessage">
+            <p class="error-message">{{ errorMessage }}</p>
+          </div>
+        </div>
         <label for="category">Category:</label>
         <select id="category" v-model="selectedCategory">
           <option v-for="category in categories" :key="category" :value="category">{{ category }}</option>
@@ -19,22 +23,40 @@
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
 import { API_BASE_URL } from '@/config/config.ts';
-import { ref } from 'vue';
 import axios from 'axios';
 
 const popupVisible = ref(false);
 const itemName = ref('');
 const selectedCategory = ref('OTHER');
 const categories = ['FRUIT', 'VEGETABLE', 'MEAT', 'FISH', 'DAIRY', 'BAKERY', 'SWEETS', 'DRINKS', 'ALCOHOL', 'OTHER'];
+const isNameValid = ref(true);
+const errorMessage = ref('');
 
 const togglePopup = () => {
   popupVisible.value = !popupVisible.value;
+  isNameValid.value = true; // Reset validation status when toggling popup
+};
+
+const validateInput = () => {
+  // Validate input name
+  const nameRegex = /^[a-zA-Z].{1,19}$/;
+  isNameValid.value = nameRegex.test(itemName.value);
 };
 
 const addItem = async () => {
-  if (itemName.value.trim() === '') return;
+  validateInput();
 
+  if (itemName.value.trim() === '') {
+    errorMessage.value = 'Please enter a valid item name.';
+    return;
+  }
+
+  if (!isNameValid.value) {
+    errorMessage.value = 'Name must start with a letter and can contain up to 20 Chars!';
+    return;
+  }
   const requestOptions = {
     method: 'POST',
     headers: {
@@ -54,6 +76,15 @@ const addItem = async () => {
   }
   togglePopup();
 };
+
+const closeErrorMessage = () => {
+  errorMessage.value = '';
+  isNameValid.value = true;
+};
+
+onMounted(() => {
+  document.body.addEventListener('click', closeErrorMessage);
+});
 </script>
 
 <style scoped>
@@ -107,4 +138,26 @@ button {
   color: #fff;
   cursor: pointer;
 }
+
+.input-container {
+  position: relative;
+}
+
+.error-cloud {
+  position: absolute;
+  top: -55px; /* Adjust as needed */
+  left: 0;
+  background-color: #fff;
+  padding: 8px;
+  border-radius: 5px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+}
+
+.error-message {
+  margin: 0;
+  color: #ff3333;
+  font-size: 12px; /* Adjust the font size as needed */
+}
+
 </style>
