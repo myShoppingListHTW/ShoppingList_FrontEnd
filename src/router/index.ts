4d08fa6d2aaf222cf9e6351137fcf9f9a7f5c331
RitaChiblaq <s0588480@htw-berlin.de>
2023-12-30 01:53:40 +0100

    fix deployment

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../components/Home.vue'
-import ShoppingList from "../components/ShoppingList.vue";
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-      {
-          path: '/ShoppingList',
-          name: 'ShoppingList',
-          component: ShoppingList
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/ShoppingList',
+            name: 'ShoppingList',
+            component: () => import('../components/ShoppingList.vue'),
+        },
 
-      },
-    {
-      path: '/Home',
-      name: 'Home',
-      component: Home
-    }
-
-  ]
+    ]
 })
 
 export default router
