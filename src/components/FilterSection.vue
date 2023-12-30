4c0edd49bb257f1e5e3671870a7c6a27195e6acb
Ghazi Nakkash <ghazi.nakkash@student.htw-Berlin.de>
2023-12-30 11:47:57 +0100

    filter in new Component

---
+++ src/components/FilterSection.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="filter-section">
+    <select :value="selectedFilterCategory" @input="updateSelectedFilterCategory">
+      <option v-for="category in categories" :key="category" :value="category">{{ category }}</option>
+    </select>
+    <button class="clear-filter-button" v-if="isFilterApplied" @click="clearFilter">Clear Filter</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    selectedFilterCategory: String,
+    categories: Array,
+  },
+  computed: {
+    isFilterApplied() {
+      return this.selectedFilterCategory !== 'all';
+    },
+  },
+  methods: {
+    updateSelectedFilterCategory(event) {
+      this.$emit('update:selectedFilterCategory', event.target.value);
+    },
+    clearFilter(event) {
+      this.$emit('update:deleteSelectedFilter', event.target.value);
+    },
+  },
+};
+</script>
+<style scoped>
+.filter-section {
+  display: flex;
+  align-items: center;
+}
+
+.filter-dropdown {
+  margin-right: 10px;
+  padding: 8px;
+  border: 1px solid #ddd;
+  color-profile: #343a40;
+  border-radius: 5px;
+  font-size: 14px;
+}
+
+.clear-filter-button {
+  background-color: #dc3545;
+  color: #fff;
+  border: none;
+  padding: 8px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 14px;
+}
+
+.clear-filter-button:hover {
+  background-color: #c82333;
+}
+
+/* Add more styles as needed */
+</style>
+
--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -1,65 +1,69 @@
 <template>
   <div>
-    <div class="filter-section">
-      <select v-model="selectedFilterCategory">
-        <option value="">All Categories</option>
-        <option v-for="category in categories" :key="category" :value="category">{{ category }}</option>
-      </select>
-      <button @click="applyFilter">Filter</button>
-    </div>
-  <div>
-    <table class="table table-responsive table-borderless table-hover table-responsive-lg">
-      <thead>
-      <tr>
-        <th scope="col" class="checkbox-col sticky-header"></th>
-        <th scope="col" class="sticky-header">Item</th>
-        <th scope="col" class="sticky-header">Status</th>
-        <th scope="col" class="sticky-header">Category</th>
-        <th scope="col" class="text-center sticky-header">Actions</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="(item, index) in items" :key="index" :class="{ 'table-success': !item.empty }">
-        <td class="checkbox-col">
-          <div class="form-check">
-            <input type="checkbox" :checked="!item.empty" @change="updateStatus(index)" class="form-check-input" />
-          </div>
-        </td>
-        <td>
-          <span :class="{ 'line-through': !item.empty }">{{ item.name }}</span>
-        </td>
-        <td>
-          <div :class="{ 'text-success': !item.empty, 'text-danger': item.empty }">
-            {{ item.empty ? 'Empty' : 'Filled' }}
-          </div>
-        </td>
-        <td>
-          <div>{{ item.category || 'NO CAT*' }}</div>
-        </td>
-        <td class="text-center">
-          <button @click="editItem(index)" class="btn btn-primary btn-sm">✎</button>
-          <button @click="deleteItem(index)" class="btn btn-danger btn-sm ml-2">✖</button>
-        </td>
-      </tr>
-      </tbody>
-    </table>
+    <FilterSection
+      :selectedFilterCategory="selectedFilterCategory"
+      :categories="categories"
+      @update:selectedFilterCategory="updateSelectedFilterCategory"
+      @update:deleteSelectedFilter="deleteSelectedFilter"
+    />
 
-    <div v-if="showEditForm" class="edit-form">
-      <h3>Edit Item</h3>
-      <input v-model="editedItem.name" placeholder="Item Name" />
-      <input v-model="editedItem.category" placeholder="Category" />
-      <button @click="saveEdits">Save</button>
-      <button @click="cancelEdit">Cancel</button>
+    <div>
+      <table class="table table-responsive table-borderless table-hover table-responsive-lg">
+        <thead>
+        <tr>
+          <th scope="col" class="checkbox-col sticky-header"></th>
+          <th scope="col" class="sticky-header">Item</th>
+          <th scope="col" class="sticky-header">Status</th>
+          <th scope="col" class="sticky-header">Category</th>
+          <th scope="col" class="text-center sticky-header">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(item, index) in items" :key="index" :class="{ 'table-success': !item.empty }">
+          <td class="checkbox-col">
+            <div class="form-check">
+              <input type="checkbox" :checked="!item.empty" @change="updateStatus(index)" class="form-check-input" />
+            </div>
+          </td>
+          <td>
+            <span :class="{ 'line-through': !item.empty }">{{ item.name }}</span>
+          </td>
+          <td>
+            <div :class="{ 'text-success': !item.empty, 'text-danger': item.empty }">
+              {{ item.empty ? 'Empty' : 'Filled' }}
+            </div>
+          </td>
+          <td>
+            <div>{{ item.category || 'NO CAT*' }}</div>
+          </td>
+          <td class="text-center">
+            <button @click="editItem(index)" class="btn btn-primary btn-sm">✎</button>
+            <button @click="deleteItem(index)" class="btn btn-danger btn-sm ml-2">✖</button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+
+      <div v-if="showEditForm" class="edit-form">
+        <h3>Edit Item</h3>
+        <input v-model="editedItem.name" placeholder="Item Name" />
+        <input v-model="editedItem.category" placeholder="Category" />
+        <button @click="saveEdits">Save</button>
+        <button @click="cancelEdit">Cancel</button>
+      </div>
     </div>
-  </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import FilterSection from '@/components/FilterSection.vue'; // Adjust the path based on your actual file structure
 import { API_BASE_URL } from '@/config/config';
 
 export default {
+  components: {
+    FilterSection,
+  },
   data() {
     return {
       items: [],
@@ -67,17 +71,26 @@ export default {
       editedItem: null,
       showEditForm: false,
       selectedFilterCategory: '', // For filtering items
-      categories: ['FRUIT', 'VEGETABLE', 'MEAT', 'FISH', 'DAIRY', 'BAKERY', 'SWEETS', 'DRINKS', 'ALCOHOL', 'OTHER']
+      categories: ['FRUIT', 'VEGETABLE', 'MEAT', 'FISH', 'DAIRY', 'BAKERY', 'SWEETS', 'DRINKS', 'ALCOHOL', 'OTHER'],
     };
   },
   mounted() {
     this.fetchItems();
   },
   methods: {
+    updateSelectedFilterCategory(category) {
+      this.selectedFilterCategory = category;
+      this.applyFilter();
+    },
+    deleteSelectedFilter() {
+      this.selectedFilterCategory = '';
+      this.applyFilter();
+    },
+
     async fetchItems() {
       try {
         const response = await axios.get(API_BASE_URL);
-        this.items = response.data.map(item => ({ ...item, empty: item.empty !== false }));
+        this.items = response.data.map((item) => ({ ...item, empty: item.empty !== false }));
         this.allItems = [...this.items]; // Populate allItems after fetching data
         this.sortItems();
       } catch (error) {
@@ -87,7 +100,7 @@ export default {
 
     applyFilter() {
       if (this.selectedFilterCategory) {
-        this.items = this.allItems.filter(item => item.category === this.selectedFilterCategory);
+        this.items = this.allItems.filter((item) => item.category === this.selectedFilterCategory);
       } else {
         this.items = [...this.allItems];
       }
@@ -102,22 +115,23 @@ export default {
     },
     deleteItem(index) {
       const itemId = this.items[index].id;
-      axios.delete(`${API_BASE_URL}/${itemId}`)
-          .then(response => {
-            if (response.status === 200) {
-              this.items.splice(index, 1);
-            } else {
-              console.error('Failed to delete item');
-            }
-          })
-          .catch(error => console.error('Error deleting item:', error));
+      axios
+        .delete(`${API_BASE_URL}/${itemId}`)
+        .then((response) => {
+          if (response.status === 200) {
+            this.items.splice(index, 1);
+          } else {
+            console.error('Failed to delete item');
+          }
+        })
+        .catch((error) => console.error('Error deleting item:', error));
     },
     editItem(index) {
       this.editedItem = { ...this.items[index] };
       this.showEditForm = true;
     },
     saveEdits() {
-      const index = this.items.findIndex(item => item.id === this.editedItem.id);
+      const index = this.items.findIndex((item) => item.id === this.editedItem.id);
       if (index !== -1) {
         this.items.splice(index, 1, this.editedItem);
         this.updateItemOnServer(this.editedItem);
@@ -126,14 +140,15 @@ export default {
       this.editedItem = null;
     },
     updateItemOnServer(item) {
-      axios.put(`${API_BASE_URL}/items/${item.id}`, item)
-          .then(response => {
-            const index = this.items.findIndex(i => i.id === item.id);
-            if (index !== -1) {
-              this.items.splice(index, 1, response.data);
-            }
-          })
-          .catch(error => console.error('Error updating item:', error));
+      axios
+        .put(`${API_BASE_URL}/items/${item.id}`, item)
+        .then((response) => {
+          const index = this.items.findIndex((i) => i.id === item.id);
+          if (index !== -1) {
+            this.items.splice(index, 1, response.data);
+          }
+        })
+        .catch((error) => console.error('Error updating item:', error));
     },
     cancelEdit() {
       this.editedItem = null;
@@ -147,10 +162,9 @@ export default {
 
       this.items.splice(index, 1, { ...item });
     },
-  }
+  },
 };
 </script>
-
 <style scoped>
 
 .edit-form {
@@ -204,4 +218,14 @@ export default {
 .checkbox-col {
   width: 20px;
 }
+.filter-section >>> .filter-dropdown {
+  margin-right: 10px;
+  padding: 8px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  font-size: 14px;
+  background-color: #f8f9fa;
+  vertical-align: middle;;
+}
+
 </style>
