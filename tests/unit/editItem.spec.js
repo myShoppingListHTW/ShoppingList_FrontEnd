b2d3174cde96dbb89fd6163604ca16c3f2f52c92
RitaChiblaq <s0588480@htw-berlin.de>
2024-01-05 09:51:26 +0100

    editItem test update

--- tests/unit/editItem.spec.js
+++ tests/unit/editItem.spec.js
@@ -2,17 +2,24 @@
  * @file editItemForm.spec.js
  * @description Unit tests for the EditItemForm.vue component.
  * Tests include editing item name and category and checking if the changes are correctly emitted.
- * @author Rita Chiblaq
+ *
  */
+
 import { shallowMount } from '@vue/test-utils';
 import EditItemForm from '@/components/EditItemForm.vue';
-import { describe, it, expect, beforeEach, vi } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 
+/**
+ * Describes a suite of tests for the EditItemForm component.
+ */
 describe('EditItemForm.vue', () => {
   let wrapper;
   let editedItem;
   let categories;
 
+  /**
+   * Before each test, shallow mount the EditItemForm component with predefined props.
+   */
   beforeEach(() => {
     editedItem = { name: 'Old Item', category: 'FRUIT' };
     categories = ['FRUIT', 'VEGETABLE', 'MEAT', 'FISH', 'DAIRY', 'BAKERY', 'SWEETS', 'DRINKS', 'ALCOHOL', 'OTHER'];
@@ -21,21 +28,23 @@ describe('EditItemForm.vue', () => {
     });
   });
 
+  /**
+   * Test for verifying if the edited item name is emitted correctly.
+   */
   it('emits the updated item when the name is edited', async () => {
     const nameInput = wrapper.find('input');
     await nameInput.setValue('New Item Name');
-
     await wrapper.find('button').trigger('click');
-
     expect(wrapper.emitted('save-edits')[0][0]).toEqual({ name: 'New Item Name', category: 'FRUIT' });
   });
 
+  /**
+   * Test for verifying if the edited item category is emitted correctly.
+   */
   it('emits the updated item when the category is edited', async () => {
     const categorySelect = wrapper.find('select');
     await categorySelect.setValue('VEGETABLE');
-
     await wrapper.find('button').trigger('click');
-
     expect(wrapper.emitted('save-edits')[0][0]).toEqual({ name: 'Old Item', category: 'VEGETABLE' });
   });
 });
