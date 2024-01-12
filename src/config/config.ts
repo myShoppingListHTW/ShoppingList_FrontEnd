9a8a3d3e7af7fbd7f10f42860b328ddc2f3ec691
Ghazi Nakkash <ghazi.nakkash@student.htw-Berlin.de>
2024-01-12 11:10:06 +0100

    fixed connection to backend

--- src/config/config.ts
+++ src/config/config.ts
@@ -10,14 +10,21 @@ async function checkAvailability(url: string): Promise<boolean> {
 }
 
 const localEndpoint = 'http://localhost:8080/api/v1/article/';
-const herokuEndpoint = 'https://myshoppinglist-5c77ab8495cc.herokuapp.com/api/v1/article';
+const herokuEndpoint = 'https://myshoppinglist-5c77ab8495cc.herokuapp.com/api/v1/article/';
 
-const localAvailable = await checkAvailability(localEndpoint);
+try {
+  const localAvailable = await checkAvailability(localEndpoint);
+  console.log('Local API available:', localAvailable);
 
-if (localAvailable) {
-  API_BASE_URL = localEndpoint;
-} else {
-  API_BASE_URL = herokuEndpoint;
+  if (localAvailable) {
+    API_BASE_URL = localEndpoint;
+  } else {
+    API_BASE_URL = herokuEndpoint;
+  }
+} catch (error) {
+  console.error('Error checking local API availability:', error);
+  API_BASE_URL = herokuEndpoint; // Fallback to Heroku in case of an error
 }
 
+console.log('API_BASE_URL:', API_BASE_URL);
 export { API_BASE_URL };
